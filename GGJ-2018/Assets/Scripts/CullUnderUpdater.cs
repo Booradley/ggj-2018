60e4e8b10b658d1ec1b330da3d25ae82f0f38f4b
Warren <warrussell@gmail.com>
2018-01-26 22:56:25 -0800

    Fixes for cull under code

--- GGJ-2018/Assets/Scripts/CullUnderUpdater.cs
+++ GGJ-2018/Assets/Scripts/CullUnderUpdater.cs
@@ -34,26 +34,27 @@ public class CullUnderUpdater : MonoBehaviour
 		if (!_renderers.Contains(renderer))
 		{
 			_renderers.Add(renderer);
+			AddCullUnderMaterial(renderer);
+		}
+	}
 
-			bool materialAlreadyExists = false;
-			foreach (Material material in renderer.materials)
+	private void AddCullUnderMaterial(Renderer renderer)
+	{
+		foreach (Material material in renderer.materials)
+		{
+			if (material.name == _instanceCullUnderMaterial.name)
 			{
-				if (material.name == _instanceCullUnderMaterial.name)
-				{
-					materialAlreadyExists = true;
-				}
-			}
-			if (!materialAlreadyExists)
-			{
-				Material[] materials = new Material[renderer.materials.Length + 1];
-				for (int i = 0; i < renderer.materials.Length; ++i)
-				{
-					materials[i] = renderer.materials[i];
-				}
-				materials[renderer.materials.Length] = _instanceCullUnderMaterial;
-				renderer.materials = materials;
+				return;
 			}
 		}
+
+		Material[] materials = new Material[renderer.materials.Length + 1];
+		for (int i = 0; i < renderer.materials.Length; ++i)
+		{
+			materials[i] = renderer.materials[i];
+		}
+		materials[renderer.materials.Length] = _instanceCullUnderMaterial;
+		renderer.materials = materials;
 	}
 
 	public void RemoveRenderer(Renderer renderer)
@@ -61,37 +62,46 @@ public class CullUnderUpdater : MonoBehaviour
 		if (_renderers.Contains(renderer))
 		{
 			_renderers.Remove(renderer);
+            RemoveCullUnderMaterial(renderer);	
+		}
+	}
 
-			bool materialAlreadyExists = false;
-			bool materialRemoved = true;
-			foreach (Material material in renderer.materials)
+	private void RemoveCullUnderMaterial(Renderer renderer)
+	{
+		bool materialAlreadyExists = false;
+		bool materialRemoved = true;
+		foreach (Material material in renderer.materials)
+		{
+			if (material.name == _instanceCullUnderMaterial.name)
 			{
-				if (material.name == _instanceCullUnderMaterial.name)
-				{
-					materialAlreadyExists = true;
-					materialRemoved = false;
-				}
-			}
-			if (materialAlreadyExists && renderer.materials.Length > 1)
-			{
-				Material[] materials = new Material[renderer.materials.Length - 1];
-				int i = 0;
-				int j = 0;
-				for (; i < renderer.materials.Length; ++i, ++j)
-				{
-					if (!materialRemoved && renderer.materials[i].name == _instanceCullUnderMaterial.name)
-					{
-						--j;
-						materialRemoved = true;
-					}
-					else
-					{
-						materials[j] = renderer.materials[i];
-					}
-				}
-				renderer.materials = materials;
+				materialAlreadyExists = true;
+				materialRemoved = false;
 			}
 		}
+		if (materialAlreadyExists && renderer.materials.Length > 1)
+		{
+			Material[] materials = new Material[renderer.materials.Length - 1];
+			int i = 0;
+			int j = 0;
+			for (; i < renderer.materials.Length; ++i, ++j)
+			{
+				if (!materialRemoved && IsCullUnderMaterial(renderer.materials[i]))
+				{
+					--j;
+					materialRemoved = true;
+				}
+				else
+				{
+					materials[j] = renderer.materials[i];
+				}
+			}
+			renderer.materials = materials;
+		}
+	}
+
+	private bool IsCullUnderMaterial(Material material)
+	{
+		return material.name == _instanceCullUnderMaterial.name || material.name == _sharedCullUnderMaterial.name;
 	}
 
 	private void Update () 
@@ -101,12 +111,13 @@ public class CullUnderUpdater : MonoBehaviour
 			if (renderer != null)
 			{ 
 				Material[] materials = renderer.materials;
-				Vector4 objectPosition = new Vector4(transform.position.x, transform.position.x, transform.position.x, 0);
 				Vector4 objectNormal = new Vector4(transform.up.x, transform.up.y, transform.up.z, 0);
 				for (int i = 0; i < materials.Length; ++i) 
 				{
-					materials[i].SetVector("_objWorldPosition", objectPosition);
-					materials[i].SetVector("_objNormal", objectNormal);
+					if (IsCullUnderMaterial(materials[i]))
+					{
+						materials[i].SetVector("_objNormal", objectNormal);
+					}
 				}
 			}
 		}
