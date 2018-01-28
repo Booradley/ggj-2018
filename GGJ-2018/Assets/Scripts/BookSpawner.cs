a2f96eb3099960d6dd5849e523bf80a5be829c87
Bradley Adams <brad@bradleyadams.com>
2018-01-28 10:10:10 -0800

    Added weighting to book spawner

--- GGJ-2018/Assets/Scripts/BookSpawner.cs
+++ GGJ-2018/Assets/Scripts/BookSpawner.cs
@@ -1,4 +1,13 @@
 ﻿using UnityEngine;
+using System;
+using System.Linq;
+
+[Serializable]
+public struct WeightedBook
+{
+    public GameObject bookPrefab;
+    public int weight;
+}
 
 public class BookSpawner : MonoBehaviour
 {
@@ -6,7 +15,7 @@ public class BookSpawner : MonoBehaviour
     private GameObject _bookGizmo;
 
     [SerializeField]
-    private GameObject[] _bookPrefabs;
+    private WeightedBook[] _bookPrefabs;
 
     [SerializeField]
     private float _spawnDistance;
@@ -21,15 +30,32 @@ public class BookSpawner : MonoBehaviour
     {
         _bookGizmo.SetActive(false);
 
-        int count = Random.Range(_minBooks, _maxBooks + 1);
+        int count = UnityEngine.Random.Range(_minBooks, _maxBooks + 1);
         float space = _spawnDistance / count;
         for (int i = 0; i < count; i++)
         {
             Vector3 pos = transform.position + transform.right * (i * space);
-            pos.x += Random.Range(0f, space - 0.03f);
-            pos.z += Random.Range(-0.005f, 0.005f);
-            Instantiate(_bookPrefabs[Random.Range(0, _bookPrefabs.Length)], pos, transform.rotation * Quaternion.Euler(new Vector3(Random.Range(80f, 100f), -90f, 0f)));
+            pos.x += UnityEngine.Random.Range(0f, space - 0.03f);
+            pos.z += UnityEngine.Random.Range(-0.005f, 0.005f);
+            Instantiate(GetRandomBook(), pos, transform.rotation * Quaternion.Euler(new Vector3(90f, -90f, 0f)));
         }
+    }
+
+    private GameObject GetRandomBook()
+    {
+        int totalWeight = _bookPrefabs.Sum(weightedBook => weightedBook.weight);
+        int roll = UnityEngine.Random.Range(0, totalWeight);
+        int currentWeight = 0;
+        for (int i = 0; i < _bookPrefabs.Length; i++)
+        {
+            currentWeight += _bookPrefabs[i].weight;
+            if (currentWeight >= roll)
+            {
+                return _bookPrefabs[i].bookPrefab;
+            }
+        }
+
+        return null;
     }
 
     private void OnDrawGizmos()
