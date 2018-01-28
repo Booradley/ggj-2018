db08e8e01c4081f9fa040fd25cb20e0f89e4afd9
Bradley Adams <brad@bradleyadams.com>
2018-01-27 20:45:53 -0800

    Added non snake stuff

---
+++ GGJ-2018/Assets/Scripts/Snaaaaaaake.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Snaaaaaaake : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _snake;
+
+    private void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    private IEnumerator Spawn()
+    {
+        WaitForSeconds interval = new WaitForSeconds(.5f);
+        while (true)
+        {
+            yield return interval;
+
+            Instantiate(_snake, transform.position, Random.rotation);
+        }
+    }
+}
