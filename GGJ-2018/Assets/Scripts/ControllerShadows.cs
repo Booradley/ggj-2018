df709e35bc6582e718881da59bd2479ee9429dea
Bradley Adams <brad@bradleyadams.com>
2018-01-28 12:49:26 -0800

    Added planet and lantern

---
+++ GGJ-2018/Assets/Scripts/ControllerShadows.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using VRTK;
+
+public class ControllerShadows : MonoBehaviour
+{
+    private IEnumerator Start()
+    {
+        GameObject leftHand = null;
+        while (leftHand == null)
+        {
+            leftHand = VRTK_DeviceFinder.GetControllerLeftHand(true);
+            yield return null;
+        }
+
+        GameObject rightHand = null;
+        while (rightHand == null)
+        {
+            rightHand = VRTK_DeviceFinder.GetControllerRightHand(true);
+            yield return null;
+        }
+
+        yield return TurnOffShadows(leftHand);
+        yield return TurnOffShadows(rightHand);
+    }
+
+    private IEnumerator TurnOffShadows(GameObject controller)
+    {
+
+        Renderer[] renderers = new Renderer[0];
+        while (renderers.Length == 0)
+        {
+            renderers = controller.GetComponentsInChildren<Renderer>();
+            yield return null;
+        }
+
+        for (int i = 0; i < renderers.Length; i++)
+        {
+            renderers[i].shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+        }
+    }
+}
