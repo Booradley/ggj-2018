a7b9a5b72dcd308ca886a11a3497c518b706edc5
Bradley Adams <brad@bradleyadams.com>
2018-01-28 13:43:39 -0800

    Added Snake Rain Book

---
+++ GGJ-2018/Assets/Scripts/SnakeRain.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using UnityEngine;
+using VRTK;
+
+public class SnakeRain : VRTK_InteractableObject
+{
+    [SerializeField]
+    private GameObject[] _snakePrefabs;
+
+    [SerializeField]
+    private Transform _spawnTransform;
+
+    private Coroutine _rainCoroutine;
+
+    public override void Grabbed(VRTK_InteractGrab currentGrabbingObject = null)
+    {
+        base.Grabbed(currentGrabbingObject);
+
+        _rainCoroutine = StartCoroutine(Spawn());
+    }
+
+    public override void Ungrabbed(VRTK_InteractGrab previousGrabbingObject = null)
+    {
+        base.Ungrabbed(previousGrabbingObject);
+
+        if (_rainCoroutine != null)
+        {
+            StopCoroutine(_rainCoroutine);
+            _rainCoroutine = null;
+        }
+    }
+
+    private IEnumerator Spawn()
+    {
+        WaitForSeconds interval = new WaitForSeconds(.5f);
+        while (true)
+        {
+            yield return interval;
+
+            GameObject go = Instantiate(_snakePrefabs[UnityEngine.Random.Range(0, _snakePrefabs.Length)], _spawnTransform.position, Random.rotation);
+            Rigidbody[] rbs = go.GetComponentsInChildren<Rigidbody>();
+            for (int i = 0; i < rbs.Length; i++)
+            {
+                rbs[i].AddForce(Random.insideUnitSphere * 0.5f);
+            }
+        }
+    }
+}
