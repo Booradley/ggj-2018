8f13858b07cbae9a97b00bedefdcb9bb04d7fde9
Bradley Adams <brad@bradleyadams.com>
2018-01-27 21:53:36 -0800

    Updated shelves

--- GGJ-2018/Assets/Scripts/BookSpawner.cs
+++ GGJ-2018/Assets/Scripts/BookSpawner.cs
@@ -22,17 +22,19 @@ public class BookSpawner : MonoBehaviour
         _bookGizmo.SetActive(false);
 
         int count = Random.Range(_minBooks, _maxBooks + 1);
+        float space = _spawnDistance / count;
         for (int i = 0; i < count; i++)
         {
-
+            Vector3 pos = transform.position + transform.right * (i * space);
+            pos.x += Random.Range(0f, space - 0.03f);
+            Instantiate(_bookPrefabs[Random.Range(0, _bookPrefabs.Length)], pos, transform.rotation * Quaternion.Euler(new Vector3(90f, -90f, 0f)));
         }
     }
 
     private void OnDrawGizmos()
     {
-        Vector3 left = transform.position + (transform.right * (_spawnDistance * -0.5f));
-        Vector3 right = transform.position + (transform.right * (_spawnDistance * 0.5f));
+        Vector3 right = transform.position + (transform.right * _spawnDistance);
 
-        Gizmos.DrawLine(left, right);
+        Gizmos.DrawLine(transform.position, right);
     }
 }
