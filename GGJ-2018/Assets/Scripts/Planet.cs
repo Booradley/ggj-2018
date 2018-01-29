75efb58648fc177655ef25a006be3bde315691a9
Bradley Adams <brad@bradleyadams.com>
2018-01-28 16:06:00 -0800

    Final changes

--- GGJ-2018/Assets/Scripts/Planet.cs
+++ GGJ-2018/Assets/Scripts/Planet.cs
@@ -2,37 +2,97 @@
 using UnityEngine;
 using VRTK;
 
-public class Planet : RigidbodyWatcher
-{	
-    [SerializeField]
-    private float _antiGravityStrength = 30f;
+public class Planet : VRTK_InteractableObject
+{
+    private Dictionary<VRTK_InteractableObject, bool> _prevValues = new Dictionary<VRTK_InteractableObject, bool>();
 
-    [System.NonSerialized]
-    protected bool _active = false;
-
-    protected override void Reset()
+    public override void Grabbed(VRTK_InteractGrab currentGrabbingObject = null)
     {
-        base.Reset();
+        base.Grabbed(currentGrabbingObject);
 
-        _active = false;
+        _prevValues.Clear();
+
+        VRTK_InteractableObject[] objects = GameObject.FindObjectsOfType<VRTK_InteractableObject>();
+        for (int i = 0; i < objects.Length; i++)
+        {
+            if (objects[i] == this)
+                continue;
+
+            if (!objects[i].IsGrabbed())
+            {
+                SetGravity(objects[i], false);
+            }
+            else
+            {
+                objects[i].InteractableObjectUngrabbed += HandleObjectDroppedNoGravity;
+            }
+        }
     }
 
-    protected override void Update()
-	{
-		base.Update();
+    public override void Ungrabbed(VRTK_InteractGrab previousGrabbingObject = null)
+    {
+        base.Ungrabbed(previousGrabbingObject);
 
-		if (IsGrabbed())
-		{
-			if (!_active)
+        VRTK_InteractableObject[] objects = GameObject.FindObjectsOfType<VRTK_InteractableObject>();
+        for (int i = 0; i < objects.Length; i++)
+        {
+            if (objects[i] == this)
+                continue;
+
+            if (!objects[i].IsGrabbed())
             {
-                for (int i = 0; i < _rigidbodyData.Length; ++i)
-                {
-                    Rigidbody rigidbody = _rigidbodyData[i].rigidbody;
-                    rigidbody.useGravity = false;
-                    rigidbody.AddForce(UnityEngine.Random.insideUnitSphere * _antiGravityStrength * rigidbody.mass);
-                }
-                _active = true;
+                SetGravity(objects[i], true);
             }
-		}
-	}
+            else
+            {
+                objects[i].InteractableObjectUngrabbed += HandleObjectDroppedGravity;
+            }
+        }
+    }
+
+    private void HandleObjectDroppedNoGravity(object sender, InteractableObjectEventArgs e)
+    {
+        VRTK_InteractableObject[] objs = e.interactingObject.GetComponentsInParent<VRTK_InteractableObject>();
+        for (int i = 0; i < objs.Length; i++)
+        {
+            objs[i].InteractableObjectUngrabbed -= HandleObjectDroppedNoGravity;
+            SetGravity(objs[i], false);
+        }
+    }
+
+    private void HandleObjectDroppedGravity(object sender, InteractableObjectEventArgs e)
+    {
+        VRTK_InteractableObject[] objs = e.interactingObject.GetComponentsInParent<VRTK_InteractableObject>();
+        for (int i = 0; i < objs.Length; i++)
+        {
+            objs[i].InteractableObjectUngrabbed -= HandleObjectDroppedGravity;
+            SetGravity(objs[i], true);
+        }
+    }
+
+    private void SetGravity(VRTK_InteractableObject obj, bool useGravity)
+    {
+        Rigidbody rb = obj.GetComponent<Rigidbody>();
+        if (rb == null)
+            return;
+
+        if (!useGravity)
+        {
+            _prevValues.Add(obj, rb.useGravity);
+
+            rb.useGravity = false;
+            rb.AddForce(UnityEngine.Random.insideUnitSphere * 30f * rb.mass);
+        }
+        else
+        {
+            if (_prevValues.ContainsKey(obj))
+            {
+                rb.useGravity = _prevValues[obj];
+            }
+            else
+            {
+                rb.useGravity = true;
+            }
+        }
+    }
 }
